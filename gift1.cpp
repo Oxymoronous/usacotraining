77aad2a440eb85d8c5ff6d1b3c2d4041315d6df1
Lai Shu Xian <sxlai2@illinois.edu>
2021-01-23 09:24:31 -0600

    20200123

--- gift1.cpp
+++ gift1.cpp
@@ -14,25 +14,26 @@ int main(){
     string array[n];
     string name;
     unordered_map<string, int> memo;
-    for(int i=0; i<n; i++){
+    for(int i=0; i<n; i++){ //this loop is initializing everyone's values to 0
         fin >> name;
         array[i]  = name;
         memo[name] = 0;
     }
+
     int buy, num; 
     for(int i=0; i<n; i++){
         string boss;
-        fin >> boss;
-        fin >> buy >> num;
+        fin >> boss;        //taking in input, this is the person that is currently buying gifts
+        fin >> buy >> num; 
         int dist = buy/num;
-        memo[boss] -= (dist*num);
+        memo[boss] -= (dist*num);  //this is the owe -ing part where the person spends money
         string frend;
         for(int i=0; i<num; i++){
             fin >> frend;
-            memo[frend] += dist;
+            memo[frend] += dist;   //this is where the friends get presents
         }
     }
     for(int i=0; i<n; i++){
-        fout << array[i] << " " << memo[array[i]] << "\n";
+        fout << array[i] << " " << memo[array[i]] << "\n"; //printing output
     }
 }
